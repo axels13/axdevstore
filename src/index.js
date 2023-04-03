1329901f3d185fef2801acdea47795aed6d4c825
axels13 <axel_cadona@hotmail.com>
2023-04-03 18:05:10 -0300

    finish

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/axels13">
+    <BrowserRouter basename="/axdevstore">
       <App />
     </BrowserRouter>{" "}
   </Provider>
